a05a6434c4f988c701be6285988534210b14911e
fgobantes <fabiangobantes@gmail.com>
2025-09-09 11:52:56 +0200

    single initialize file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Demo of Distributed MINDFul.jl with TFS controller inside LXD virtual machines
 
-Initialize the different IBN-Frameworks by including the corresponding Julia file
+Include `initialize.jl` and instatiate the different IBN-Frameworks by calling the corresponding function depending wether to use TFS or not
 
-It will return the corresponing `ibnf`
+Each one will return the corresponing `ibnf`
 
 An example of the operation and ploting of a connectivity intent is provided in `intent.jl`
--- initialize.jl
+++ initialize.jl
@@ -11,6 +11,16 @@ const MINDFM = MINDFulMakie
 
 import MINDFul: ReturnCodes, IBNFramework, getibnfhandlers, GlobalNode, ConnectivityIntent, addintent!, NetworkOperator, compileintent!, KShorestPathFirstFitCompilation, installintent!, uninstallintent!, uncompileintent!, getidag, getrouterview, getoxcview, RouterPortLLI, TransmissionModuleLLI, OXCAddDropBypassSpectrumLLI, canreserve, reserve!, getlinkspectrumavailabilities, getreservations, unreserve!, getibnfid, getidagnodestate, IntentState, getidagnodechildren, getidagnode, OpticalTerminateConstraint, getlogicallliorder, issatisfied, getglobalnode, getibnag, getlocalnode, getspectrumslotsrange, gettransmissionmode, getname, gettransmissionmodule, TransmissionModuleCompatibility, getrate, getspectrumslotsneeded, OpticalInitiateConstraint, getnodeview, getnodeview, getsdncontroller, getrouterview, removeintent!, getlinkstates, getcurrentlinkstate, setlinkstate!, logicalordercontainsedge, logicalordergetpath, edgeify, getintent, RemoteIntent, getisinitiator, getidagnodeid, getibnfhandler, getidagnodes, @passtime, getlinkstates, GBPSf
 
+using MINDFulTeraFlowSDN
 
-push!(ARGS, "data/config2.toml")
-ibnf2 = MINDFul.main()
+
+function initializewithTFS(configfile::String, tfs_ip::String)
+    push!(ARGS, configfile)
+    push!(ARGS, tfs_ip)
+    return MINDFulTeraFlowSDN.main()
+end
+
+function initializewithoutTFS(configfile::String)
+    push!(ARGS, configfile)
+    return MINDFul.main()
+end
