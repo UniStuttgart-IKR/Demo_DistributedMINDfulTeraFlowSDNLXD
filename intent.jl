9fb6a793508592b463b67a00a8a5fd725a09f919
fgobantes <fabiangobantes@gmail.com>
2025-09-09 11:22:38 +0200

    demo prep

---
+++ intent.jl
@@ -0,0 +1,18 @@
+#Connectivity Intent between two nodes in different domains
+conintent_neigh = MINDF.ConnectivityIntent(MINDF.GlobalNode(UUID(1), 4), MINDF.GlobalNode(UUID(3), 47), u"100.0Gbps")
+intentuuid_neigh = MINDF.addintent!(ibnf1, conintent_neigh, MINDF.NetworkOperator())
+
+#Compile the intent
+MINDF.compileintent!(ibnf1, intentuuid_neigh, MINDF.KShorestPathFirstFitCompilation(5))
+
+#Plot the intent
+intentplot(ibnf1, intentid = intentuuid_neigh; multidomain=true, showstate = true, showintent = true)
+
+#Check the low-level implementation of the intent
+MINDF.getlogicallliorder(ibnf1, intentuuid_neigh; onlyinstalled=false)
+
+#Plot the whole graph with the intent highlighted
+ibnplot(ibnf1; multidomain=true, intentids = [intentuuid_neigh], shownodelabels = :local)
+
+#Install the intent
+MINDF.installintent!(ibnf1, intentuuid_neigh; verbose=true)
