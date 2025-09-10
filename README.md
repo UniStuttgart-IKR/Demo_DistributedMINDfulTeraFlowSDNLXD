a236db3e771ae931edbac8d68a141d82f507b367
fgobantes <fabiangobantes@gmail.com>
2025-09-10 08:20:36 +0200

    .

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Demo of Distributed MINDFul.jl with TFS controller inside LXD virtual machines
+# Demo of Distributed MINDFul.jl with TFS controllers inside LXD virtual machines
 
 Include `initialize.jl` and instatiate the different IBN-Frameworks by calling the corresponding function depending on wether to use TFS or not
 
@@ -7,3 +7,7 @@ Both functions expect the configuration file in TOML format. TFS also expects th
 Each one will return the corresponing `ibnf`
 
 An example of the operation and ploting of a connectivity intent is provided in `intent.jl`
+
+### How to generate RSA keys if needed
+`openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv1.pem`
+`openssl pkey -in rsa_priv1.pem -out rsa_pub1.pem -pubout`
--- intent.jl
+++ intent.jl
@@ -1,25 +1,51 @@
+#Plot the multidomain graph
+ibnplot(ibnf1; multidomain=true, shownodelabels = :local)
+
 #Connectivity Intent between a node from domain 1 and a node from domain 3
-conintent_neigh = MINDF.ConnectivityIntent(MINDF.GlobalNode(UUID(1), 4), MINDF.GlobalNode(UUID(3), 47), u"100.0Gbps")
-intentuuid_neigh = MINDF.addintent!(ibnf1, conintent_neigh, MINDF.NetworkOperator())
+intent = MINDF.ConnectivityIntent(MINDF.GlobalNode(UUID(1), 4), MINDF.GlobalNode(UUID(3), 47), u"100.0Gbps")
 
-#Compile the intent
-MINDF.compileintent!(ibnf1, intentuuid_neigh, MINDF.KShorestPathFirstFitCompilation(5))
+#Add the intent to the IBNFramework
+intentid = MINDF.addintent!(ibnf1, intent, MINDF.NetworkOperator())
 
-#Plot the intent
-intentplot(ibnf1, intentid = intentuuid_neigh; multidomain=true, showstate = true, showintent = true)
-
-#Check the low-level implementation of the intent
-MINDF.getlogicallliorder(ibnf1, intentuuid_neigh; onlyinstalled=false)
+#Compile the intent with a especific algorithm
+MINDF.compileintent!(ibnf1, intentid, MINDF.KShorestPathFirstFitCompilation(5))
 
 #Plot the whole graph with the intent highlighted
-ibnplot(ibnf1; multidomain=true, intentids = [intentuuid_neigh], shownodelabels = :local)
+ibnplot(ibnf1; multidomain=true, intentids = [intentid], shownodelabels = :local)
+
+#Plot the intent
+f,_,_=intentplot(ibnf1, intentid = intentid; multidomain=true, showstate = true, showintent = true); display(GLMakie.Screen(), f);
 
 #Install the intent
-MINDF.installintent!(ibnf1, intentuuid_neigh; verbose=true)
+MINDF.installintent!(ibnf1, intentid; verbose=true)
+
+
+
+
+
+
+
+
+#Check the low-level implementation of the intent
+# MINDF.getlogicallliorder(ibnf1, intentuuid_neigh; onlyinstalled=false)
+
+
+# RouterPortLLI(4, 61)
+#  TransmissionModuleLLI(4, 11, 1, 61, 1)
+#  OXCAddDropBypassSpectrumLLI(4, 0, 1, 1, 1:4)
+#  OXCAddDropBypassSpectrumLLI(1, 4, 0, 22, 1:4)
+#  OXCAddDropBypassSpectrumLLI(22, 1, 0, 21, 1:4)
+#  OXCAddDropBypassSpectrumLLI(21, 22, 0, 23, 1:4)
+#  OXCAddDropBypassSpectrumLLI(23, 21, 0, 17, 1:4)
+#  OXCAddDropBypassSpectrumLLI(17, 23, 0, 29, 1:4)
+
 
 
 #Uninstall the intent
-MINDF.uninstallintent!(ibnf1, intentuuid_neigh)
+MINDF.uninstallintent!(ibnf1, intentid)
 
 #Uncompile the intent
-MINDF.uncompileintent!(ibnf1, intentuuid_neigh)
+MINDF.uncompileintent!(ibnf1, intentid)
+
+#Remove the intent
+MINDF.removeintent!(ibnf1, intentid)
